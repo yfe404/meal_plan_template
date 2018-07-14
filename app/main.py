1e683a08e0950e50f7bc2ab8df05dc1173003a38
yafeunteun <yannfeunteun@gmail.com>
2018-07-14 20:26:04 +0200

    Add recipe steps

--- app/main.py
+++ app/main.py
@@ -3,26 +3,32 @@ import pdfkit
 
 app = Flask(__name__)
 
+data = {
+    "calories": 500,
+    "proteins": 42,
+    "carbohydrates": 39,
+    "fats": 19,
+    "ingredients": [
+        {"name": "Oeuf entier", "quantity": 3, "unit": "unit"},
+        {"name": "Blanc de poulet", "quantity": 80, "unit": "g"},
+        {"name": "Poivron", "quantity": 80, "unit": "g"},
+        {"name": "Champignons de Paris", "quantity": 80, "unit": "g"},
+        {"name": "Riz complet", "quantity": 50, "unit": "g"},
+    ],
+    "name": "Omelette", "steps": [
+        "Dans un grand recipient: fouettez le oeufs. Vous pouvez ajouter du poivres et des épices selon vos préferences.",
+        "Dans un plat, coupez le blanc de poulet et les poivrons en dés.",
+        "Versez dans une poêle chaude votre mélange d'oeufs fouettés.",
+        "Ajoutez-y le poulet et les poivrons.",
+        "Laissez cuire jusqu'à ce que l'omelette soit prête.",
+        "Pour la cuisson du riz, référez-vous aux instructions présentes sur l'emballage (typiquement environ 10min dans une casserole d'eau bouillante)."
+    ], "type": ['lunch', 'dinner']
+}
 
 @app.route('/')
 def index():
-
-    data = {
-        "calories": 500,
-        "proteins": 42,
-        "carbohydrates": 39,
-        "fats": 19,
-        "ingredients": [
-            {"name": "Oeuf entier", "quantity": 3, "unit": "g"},
-            {"name": "Blanc de poulet", "quantity": 80, "unit": "g"},
-            {"name": "Poivron", "quantity": 80, "unit": "g"},
-            {"name": "Champignons de Paris", "quantity": 80, "unit": "g"},
-            {"name": "Riz complet", "quantity": 50, "unit": "g"},
-        ],
-        "name": "Omelette", "steps": ["<RECIPE_STEPS>"], "type": ['lunch', 'dinner']
-    }
     
-    rendered = render_template('pdf_template.html')
+    rendered = render_template('pdf_template.html', data=data)
     pdf = pdfkit.from_string(rendered, False)
 
     response = make_response(pdf) 
@@ -35,7 +41,7 @@ def index():
 
 @app.route('/test')
 def test():
-    return render_template('pdf_template.html')
+    return render_template('pdf_template.html', data=data)
 
 
 if __name__ == '__main__':

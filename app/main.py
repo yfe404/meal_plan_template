3f80a40fb0122762238562d2c56a6d796157a814
yafeunteun <yannfeunteun@gmail.com>
2018-07-14 17:40:49 +0200

    sample data

--- app/main.py
+++ app/main.py
@@ -6,6 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+
+    data = {
+        "calories": 500,
+        "proteins": 42,
+        "carbohydrates": 39,
+        "fats": 19,
+        "ingredients": [
+            {"name": "Oeuf entier", "quantity": 3, "unit": "g"},
+            {"name": "Blanc de poulet", "quantity": 80, "unit": "g"},
+            {"name": "Poivron", "quantity": 80, "unit": "g"},
+            {"name": "Champignons de Paris", "quantity": 80, "unit": "g"},
+            {"name": "Riz complet", "quantity": 50, "unit": "g"},
+        ],
+        "name": "Omelette", "steps": ["<RECIPE_STEPS>"], "type": ['lunch', 'dinner']
+    }
+    
     rendered = render_template('pdf_template.html')
     pdf = pdfkit.from_string(rendered, False)
 
@@ -27,3 +43,5 @@ if __name__ == '__main__':
                       
                     
 
+
+    
